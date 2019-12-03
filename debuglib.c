3cb45200f5ca548529ae193e54e60688c11b68e8
Polterino <mgusella2@icloud.com>
2019-12-03 11:13:22 +0100

    Avanzamento della libreria

--- debuglib.c
+++ debuglib.c
@@ -1,15 +1,67 @@
 /* Michele Gusella 5AI
  * Libreria per testare i programma in corso di creazione
+ * 
+ * 5 Critical
+ * 4 Error
+ * 3 Warning
+ * 2 Info
+ * 1 Log
+ * 
+ * Lista funzioni:
+ * 		filedebuglevel(nomefile)					Al posto che stampare l'errore in stderr, stamparlo in un file
+ * 		printdebuglevel(livello, messaggio)			Stampa l'errore se il livello passato è >= del currentdebuglevel
+ * 		setdebuglevel(lvl)							Cambia il currentdebuglevel
+ * 		pushandsetdebuglevel(lvl)					Salva il livello corrente e lo cambia con quello passato in input
+ * 		restoredebuglevel()							Ripristina il livello precedentemente salvato da un pushandsetdebuglevel, sempre se c'e ne uno
  */
 
 
 #include <stdio.h>
+#include <string.h>
 
-int currentdebuglevel = 4;
+// STAMPARE I MESSAGGI IN STDERR -> STANDARD ERROR
+int currentdebuglevel = 4, salvato = -1;
+char *nomefile = "";
+FILE *file;
 
-#ifdef DEBUG_TEST // test 
-int main(int argc, char **argv)
+void filedebuglevel(char *ilfile);
+void printdebuglevel(int lvl);
+void setdebuglevel(int lvl);
+
+void filedebuglevel(char *ilfile)
+{
+	nomefile = ilfile;
+}
+
+void printdebuglevel(int lvl, char *mesg)
+{
+	if(lvl >= currentdebuglevel)
+	{
+		if(nomefile == "")
+			fprintf(stderr, mesg);
+		else
+		{
+			file = fopen(nomefile, "w");
+			fprintf(file, mesg);
+			fclose(file);
+		}
+	}
+}
+
+void setdebuglevel(int lvl)
+{
+	currentdebuglevel = lvl;
+}
+
+void pushandsetdebuglevel(int lvl)
 {
 	
 }
-#endif // DEBUG_TEST
+
+void restoredebuglevel()
+{
+	if(salvato != -1)
+		currentdebuglevel = salvato;
+	else
+		printf("Attenzione! Il restore ha riscontrato un problema\n");
+}
